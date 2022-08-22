d171e2cec4dc581d797151fcc8aa2278f4ebb40b
youns-nibras <103293719+youns-nibras@users.noreply.github.com>
2022-08-22 16:27:41 +0300

    Update find_all_prime.py

--- find_all_prime.py
+++ find_all_prime.py
@@ -1,1 +1,10 @@
-
+lower_value = int(input("Please, Enter Lowest Range Value: "))
+upper_value = int(input("Please, Enter Upper Range Value: "))
+print("the prime number in the range are: ".capitalize())
+for number in range(lower_value,upper_value+1):
+    if number > 1: 
+        for i in range(2,number):
+            if (number % i) == 0:
+                break
+        else:
+            print(number)
